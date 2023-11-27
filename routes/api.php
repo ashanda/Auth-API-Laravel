d59baef2f7bbd91f1730cd4d16f8d203027e2d0c
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-11-27 16:53:18 +0530

    login,register token auth and get user details

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RegisterController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+Route::post('/login', [LoginController::class,'login']);
+Route::post('/register', [RegisterController::class,'register']);
+
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/profile', [ProfileController::class,'show']);
+    Route::post('/logout', [ProfileController::class,'logout']);
+    Route::get('/permissions', [PermissionController::class,'index']);
+});
+
